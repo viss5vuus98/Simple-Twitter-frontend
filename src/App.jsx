dbdf1adee526260f08d7b852a8260f4ea8a25821
violet7755 <violet7755@gmail.com>
2022-12-15 01:49:41 +0800

    router test

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.scss';
 import './basicStyle.scss'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { MainPage } from 'pages'
+import { MainPage, LoginPage, RegisterPage } from 'pages';
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
-          <Route path="*" element={<MainPage/>}/>
+          <Route path="*" element={<MainPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
         </Routes>
       </BrowserRouter>
     </div>
