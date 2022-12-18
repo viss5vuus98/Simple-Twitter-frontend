dc18495844cf3ef405b4bd68034cbf4836433331
viss5vuus98 <dokipu@gmailcom>
2022-12-18 20:14:54 +0800

    ADD FollowingPage 完成FollowPage切版

--- src/pages/Main/FollowingPage.jsx
+++ src/pages/Main/FollowingPage.jsx
@@ -1,8 +1,19 @@
 import { FollowingList, FollowingTab } from 'components';
+//style
+import style from './midContent.module.scss'
+//icon
+import { arrow } from '../../assets/images/index';
 
 const FollowingPage = () => {
   return (
     <>
+      <div className={style.header}>
+        <img className={style.arrow} src={arrow} alt="" />
+        <div className={style.self}>
+          <h5 className={style.userName}>Joho Doe</h5>
+          <p className={style.tweetCount}>25 推文</p>
+        </div>        
+      </div>
       <FollowingTab/>
       <FollowingList/>
     </>
