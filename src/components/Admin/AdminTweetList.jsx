40ef34f085ed92d55d5ac92acaa648d4dfd0ca04
viss5vuus98 <dokipu@gmailcom>
2022-12-19 23:17:18 +0800

    AdminTweet

---
+++ src/components/Admin/AdminTweetList.jsx
@@ -0,0 +1,29 @@
+import style from './adminTweet.module.scss'
+//asset
+import { avatar } from '../../assets/images/index';
+import close from '../../assets/images/closeGray.svg'
+
+const AdminTweet = () => {
+  return (
+    <div className={style.tweetItem}>
+      <div className={style.avatar}>
+        <img src={avatar} alt="" />
+      </div>
+      <div className={style.tweetBody}>
+        <div className={style.tweetTitle}>
+          <p>John</p>
+          <span>
+            @apple 3小時前
+          </span>
+        </div>
+        <p className={style.tweetText}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laudantium dolorem nostrum? Distinctio, harum et.</p>
+      </div>
+      <div className={style.delete}>
+        <button className={style.close} style={{ backgroundImage: `url(${close})` }}>
+        </button>        
+      </div>
+    </div>
+  )
+}
+
+export default AdminTweet;
