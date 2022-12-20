2dca029ca49dc01c38c0020a8696c41349c3c771
violet7755 <violet7755@gmail.com>
2022-12-20 22:28:50 +0800

    add logout function

--- src/components/Common_/Navbar.jsx
+++ src/components/Common_/Navbar.jsx
@@ -2,6 +2,7 @@ import style from './nav.module.scss';
 import logo from '../../assets/images/ACLogoIcon.svg';
 import { logout} from '../../assets/images/index'
 import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const AdminNavBar = ({funItems, isAdmin}) => {
   const navLinks = funItems.map(item => {
@@ -10,30 +11,39 @@ const AdminNavBar = ({funItems, isAdmin}) => {
               <NavLink to={item.link}>{item.name}</NavLink>
             </li>)
   })
+  const navigate = useNavigate();
 
+  const handleClick = () => {
+    localStorage.removeItem('authToken');
+    navigate('/login');
+  };
 
   return (
-  <div className={style.sidebar}>
-    <div className={style.header}>
-      <div className={style.logo}>
-        <img src={logo} alt="" />
+    <div className={style.sidebar}>
+      <div className={style.header}>
+        <div className={style.logo}>
+          <img src={logo} alt="" />
+        </div>
       </div>
-    </div>
-    <nav>
-      <ul className={style.navItems}>
-        {navLinks}
-        {/* <li className={style.navLink}><img className={style.icon} src={home} alt="" />首頁</li>
+      <nav>
+        <ul className={style.navItems}>
+          {navLinks}
+          {/* <li className={style.navLink}><img className={style.icon} src={home} alt="" />首頁</li>
         <li className={style.navLink}><img className={style.icon} src={user} alt="" />個人資料</li>
         <li className={style.navLink}><img className={style.icon} src={set} alt="" />設定</li> */}
-        {!isAdmin && <li className={style.control}><button className={style.modalBtn}>推文</button></li>}
-      </ul>
-    </nav>
-    <div className={style.navFooter}>
-          <img src={logout} alt="" />
-          登出
+          {!isAdmin && (
+            <li className={style.control}>
+              <button className={style.modalBtn}>推文</button>
+            </li>
+          )}
+        </ul>
+      </nav>
+      <button className={style.navFooter} onClick={handleClick}>
+        <img src={logout} alt="" />
+        登出
+      </button>
     </div>
-  </div>
-  )
+  );
 }
 
 export default AdminNavBar
